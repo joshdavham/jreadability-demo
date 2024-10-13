4a6dc693ae78d279dad89cfff729d1b5b038850c
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2024-10-13 14:46:29 -0500

    add favicon and logo

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import streamlit as st
 from jreadability import compute_readability
 
 st.set_page_config(
-    page_title='jreadability demo'
+    page_title='jReadability demo',
+    page_icon='favicon.svg',
 )
 
 lower_intermediate_text = "茶々はわたしの日本語の先輩でした。毎日、日本語を一生懸命勉強して3か月ぐらいたったころには、日本人の友達といろいろな話もできるようになりました。そして茶々よりも日本語がわかるようになりました。けれどわたしの日本語の発音はまだ上手ではありません。家の人はわかってくれますが、茶々はわかりません。「おいで」と言っても茶々は来ません。「ちょっと見てごらん」と言っても見ません。「散歩に行こう」と言ってもふりむいてくれません。だから、いっしょに散歩することもできません。わたしの発音が家の人とちがうからです。それが大変ざんねんでした。それで、わたしはテープをたくさん聞いて練習しました。そして、はじめて茶々がわたしの「おいで」を聞いて、わたしのところへ来てくれた時は、本当にうれしくなりました。\
     わたしの日本語がやっと茶々につうじたからです。今は、わたしは日本の生活にだいぶなれました。茶々はもう、わたしの日本語が大体わかりますから、毎日いっしょに散歩します。わたしの日本語の先輩、茶々のおかげで日本語も上手になったし、犬がこわくなくなって、犬が好きになりました。今度、わたしは茶々に中国語を教えようと思います。そして、中国語と日本語と、二つのことばのわかる犬にするつもりです。"
 
-st.markdown('# jreadability demo')
+st.image("logo.svg", use_column_width=False, width=350)
 
 text = st.text_area(
     label="Input Japanese text below",
