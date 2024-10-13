770d00eaa03f355d5793c9088ec7ecff971a021b
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2024-10-13 15:55:46 -0500

    add button

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ st.set_page_config(
     page_icon='favicon.svg',
 )
 
-st.markdown("[Code for jreadability python [package](https://github.com/joshdavham/jreadability)]")
-st.markdown("[Code for this [demo](https://github.com/joshdavham/jreadability-demo)]")
+st.markdown("[Code for jreadability python [package](https://github.com/joshdavham/jreadability) and [demo](https://github.com/joshdavham/jreadability-demo)]")
 
 lower_intermediate_text = "茶々はわたしの日本語の先輩でした。毎日、日本語を一生懸命勉強して3か月ぐらいたったころには、日本人の友達といろいろな話もできるようになりました。そして茶々よりも日本語がわかるようになりました。けれどわたしの日本語の発音はまだ上手ではありません。家の人はわかってくれますが、茶々はわかりません。「おいで」と言っても茶々は来ません。「ちょっと見てごらん」と言っても見ません。「散歩に行こう」と言ってもふりむいてくれません。だから、いっしょに散歩することもできません。わたしの発音が家の人とちがうからです。それが大変ざんねんでした。それで、わたしはテープをたくさん聞いて練習しました。そして、はじめて茶々がわたしの「おいで」を聞いて、わたしのところへ来てくれた時は、本当にうれしくなりました。\
     わたしの日本語がやっと茶々につうじたからです。今は、わたしは日本の生活にだいぶなれました。茶々はもう、わたしの日本語が大体わかりますから、毎日いっしょに散歩します。わたしの日本語の先輩、茶々のおかげで日本語も上手になったし、犬がこわくなくなって、犬が好きになりました。今度、わたしは茶々に中国語を教えようと思います。そして、中国語と日本語と、二つのことばのわかる犬にするつもりです。"
@@ -15,7 +14,7 @@ lower_intermediate_text = "茶々はわたしの日本語の先輩でした。�
 st.image("logo.svg", use_column_width=False, width=350)
 
 text = st.text_area(
-    label="Input Japanese text below",
+    label="Input Japanese text below and then click the button",
     label_visibility='visible',
     height=350,
     max_chars=None,
@@ -24,30 +23,37 @@ text = st.text_area(
     value = lower_intermediate_text,
 )
 
-if text:
+if st.button("Compute readability"):
 
-    score = compute_readability(text)
+    if text:
 
-    if 0.5 <= score < 6.5:
+        score = compute_readability(text)
 
-        if 0.5 <= score < 1.5:
-            st.error(f"Readability score: **{score:.2f}**  \nLevel: **Upper-advanced**")
-        elif 1.5 <= score < 2.5:
-            st.error(f"Readability score: **{score:.2f}**  \nLevel: **Lower-advanced**")
-        elif 2.5 <= score < 3.5:
-            st.info(f"Readability score: **{score:.2f}**  \nLevel: **Upper-intermediate**")
-        elif 3.5 <= score < 4.5:
-            st.info(f"Readability score: **{score:.2f}**  \nLevel: **Lower-intermediate**")
-        elif 4.5 <= score < 5.5:
-            st.success(f"Readability score: **{score:.2f}**  \nLevel: **Upper-elementary**")
-        elif 5.5 <= score < 6.5:
-            st.success(f"Readability score: **{score:.2f}**  \nLevel: **Lower-elementary**")
+        if 0.5 <= score < 6.5:
 
-        st.progress(1-((score - 0.5) / 6.0) )
+            if 0.5 <= score < 1.5:
+                st.error(f"Readability score: **{score:.2f}**  \nLevel: **Upper-advanced**")
+            elif 1.5 <= score < 2.5:
+                st.error(f"Readability score: **{score:.2f}**  \nLevel: **Lower-advanced**")
+            elif 2.5 <= score < 3.5:
+                st.info(f"Readability score: **{score:.2f}**  \nLevel: **Upper-intermediate**")
+            elif 3.5 <= score < 4.5:
+                st.info(f"Readability score: **{score:.2f}**  \nLevel: **Lower-intermediate**")
+            elif 4.5 <= score < 5.5:
+                st.success(f"Readability score: **{score:.2f}**  \nLevel: **Upper-elementary**")
+            elif 5.5 <= score < 6.5:
+                st.success(f"Readability score: **{score:.2f}**  \nLevel: **Lower-elementary**")
+
+            st.progress(1-((score - 0.5) / 6.0) )
+
+        else:
+
+            st.warning(f"Readability score: **{score:.2f}**  \nLevel: **Undefined**") 
 
     else:
 
-        st.warning(f"Readability score: **{score:.2f}**  \nLevel: **Undefined**") 
+        st.info(f"Readability score:  \nLevel:")
+        st.progress(0.0)
 
 else:
 
